7b33f2d12db960116b0d8b218f4bb26b671dd1b3
Alimoff <sobiriddin@gmail.com>
2022-07-02 12:09:19 +0500

    add:news model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class News(models.Model):
+    name = models.CharField("Text Title,", max_length=200)
+    text = models.TextField("Text Area")
+
+    # more info
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Text"
+        verbose_name_plural = "More Text"
